224ca48a73a594076dc479614fd25436ede56c61
zicc <ziconakano00@gmail.com>
2025-10-01 14:43:47 +0700

    update #3
    update #3

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,11 @@
-import pytest
 from src.app import add, divide
-
+import pytest
 
 def test_add():
     assert add(2, 3) == 5
 
-
 def test_divide():
     assert divide(10, 2) == 5
-
 
 def test_divide_by_zero():
     with pytest.raises(ValueError, match="Tidak boleh bagi nol"):
