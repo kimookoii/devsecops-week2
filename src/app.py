fdb4d9beaeeded3f0d64e345d104846ffb881d8e
zicc <ziconakano00@gmail.com>
2025-10-01 14:39:18 +0700

    update #1
    update #1

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 def add(a, b):
     return a + b
+
+
 def divide(a, b):
     if b == 0:
-    raise ValueError("Tidak boleh bagi nol")
+        raise ValueError("Tidak boleh bagi nol")
     return a / b
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,15 @@
-from src.app import add, divide
 import pytest
+from src.app import add, divide
+
 
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_divide():
     assert divide(10, 2) == 5
 
+
 def test_divide_by_zero():
-    try:
+    with pytest.raises(ValueError, match="Tidak boleh bagi nol"):
         divide(10, 0)
-    except ValueError as e:
-        assert str(e) == "Tidak boleh bagi nol"
