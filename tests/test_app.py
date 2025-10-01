6d87dd083b3d5871d549316ffa73fdb142a8f1fd
zicc <ziconakano00@gmail.com>
2025-10-01 14:45:34 +0700

    update #4
    update #4

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-from src.app import add, divide
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from app import add, divide
 import pytest
 
 def test_add():
